150463c30c9f6d78f8f9601d54bb3cc2fd3c5ea0
kiefer <eero.manty@gmail.com>
2020-06-06 15:08:56 +0300

    add Index and IndexMut for Grid

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,39 @@
+use std::ops::{Index, IndexMut};
+
 #[derive(Clone)]
 struct Grid<D> {
-    width: u32,
-    height: u32,
+    width: usize,
+    height: usize,
     data: Vec<D>,
 }
 
 impl<D: Clone> Grid<D> {
-    pub fn init(init_data: &[D], width: u32) -> Option<Grid<D>> {
-        let size = init_data.len() as u32;
+    pub fn init(init_data: &[D], width: usize) -> Option<Grid<D>> {
+        let size = init_data.len() ;
         if size % width != 0 {
             None
         } else {
             Some(Grid { width, height: size / width, data: init_data.to_vec() })
         }
+    }
+}
+
+impl<D> Index<(usize, usize)> for Grid<D> {
+    type Output = D;
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        let x = index.0 % self.width;
+        let y = index.1 % self.height;
+
+        &self.data[y * (self.width as usize) * x]
+    }
+}
+
+impl<D> IndexMut<(usize, usize)> for Grid<D>{
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        let x = index.0 % self.width;
+        let y = index.1 % self.height;
+
+        &mut self.data[y * (self.width as usize) * x]
     }
 }
 
@@ -22,8 +43,7 @@ pub struct Game<D> {
 }
 
 impl<D: Clone> Game<D> {
-    pub fn new(init_data: &[D], width: u32) -> Option<Game<D>> {
+    pub fn new(init_data: &[D], width: usize) -> Option<Game<D>> {
         Grid::init(init_data, width).map_or(None, |grid| Some(Game { grid }))
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,4 @@ use game_of_life::{Game};
 
 fn main() {
     let game = Game::new(&[false; 600 * 600],600).unwrap();
-    println!("Hello, world!");
 }
