37840e8bcd47a10d91c4d8cb59d2a86391efe1be
kiefer <eero.manty@gmail.com>
2020-06-06 22:03:06 +0300

    Graphics works, with history

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,41 @@
 use std::ops::{Index, IndexMut};
+use simple::{Window, Rect};
 
 type IndexType = (i32, i32);
+
+pub trait RuleSet<D> {
+    fn next(&self, source: &[&D]) -> D;
+    fn source_size(&self) -> u8;
+}
+
+pub trait DataType: Clone {
+    fn get_color(&self) -> (u8, u8, u8, u8);
+    fn get_char(&self) -> char;
+}
 
 #[derive(Clone)]
 struct Grid<D> {
     width: u32,
     height: u32,
     data: Vec<D>,
-
 }
 
-impl<D: Clone> Grid<D> {
+impl<D: DataType> Grid<D> {
     pub fn init(init_data: &[D], width: u32) -> Option<Grid<D>> {
-        let size = init_data.len() as u32;
-        if size % width != 0 {
+        let size = init_data.len();
+        let uw = width as usize;
+        if size % uw != 0 {
             None
         } else {
-            Some(Grid { width, height: size / width, data: init_data.to_vec() })
+            Some(Grid { width, height: (size / uw) as u32, data: init_data.to_vec() })
         }
     }
 
     fn wrap(&self, index: IndexType) -> (usize, usize) {
-        (index.0.rem_euclid( self.width as i32) as usize, index.1.rem_euclid( self.width as i32) as usize)
+        (index.0.rem_euclid(self.width as i32) as usize, index.1.rem_euclid(self.height as i32) as usize)
     }
 
-    fn get_area(&self, index: IndexType, size: u32) -> Vec<&D> {
+    fn get_area(&self, index: IndexType, size: u8) -> Vec<&D> {
         let mut v = Vec::new();
         let x = index.0 as i32;
         let y = index.1 as i32;
@@ -36,9 +47,19 @@ impl<D: Clone> Grid<D> {
         }
         v
     }
+
+    fn print(&self){
+        for (i,v) in self.data.iter().enumerate() {
+            print!("{}",v.get_char());
+            if (i+1) % self.width as usize  == 0{
+                print!("\n");
+            }
+        }
+
+    }
 }
 
-impl<D: Clone> Index<IndexType> for Grid<D> {
+impl<D: DataType> Index<IndexType> for Grid<D> {
     type Output = D;
     fn index(&self, index: IndexType) -> &Self::Output {
         let (x, y) = self.wrap(index);
@@ -46,38 +67,74 @@ impl<D: Clone> Index<IndexType> for Grid<D> {
     }
 }
 
-impl<D: Clone> IndexMut<IndexType> for Grid<D> {
+impl<D: DataType> IndexMut<IndexType> for Grid<D> {
     fn index_mut(&mut self, index: IndexType) -> &mut Self::Output {
         let (x, y) = self.wrap(index);
         &mut self.data[y * (self.width as usize) + x]
     }
 }
 
-pub trait RuleSet<D>{
-    fn update(&self, source:&[&D]) -> D;
-    fn source_size(&self) -> u32;
-}
 
-
-pub struct Game<D, R>
-    where R: RuleSet<D>{
+pub struct Game<D, R> {
     grid: Grid<D>,
     rules: R,
 }
 
-impl<D: Clone, R> Game<D, R>
-    where R: RuleSet<D> {
-    pub fn new(init_data: &[D], width: u32, rules: R) -> Option<Game<D,R>> {
+struct Coord{
+    width:u32,
+    height:u32,
+    x:u32,
+    y:u32,
+}
+
+impl Iterator for Coord {
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let o_x = self.x as i32;
+        let o_y = self.y as i32;
+
+        if self.y >= self.height {
+            None
+        } else {
+            self.x += 1;
+            if self.x >= self.width {
+                self.x -= self.width;
+                self.y += 1;
+            }
+            Some((o_x, o_y))
+        }
+    }
+}
+
+impl<D, R> Game<D, R>
+    where D: DataType,
+          R: RuleSet<D> {
+    pub fn new(init_data: &[D], width: u32, rules: R) -> Option<Game<D, R>> {
         Grid::init(init_data, width).map_or(None, |grid| Some(Game { grid, rules }))
     }
 
     pub fn next(&mut self) {
         let grid_copy = self.grid.clone();
-        for y in 0..grid_copy.height as i32{
-            for x in 0..grid_copy.width as i32{
-                let area = grid_copy.get_area((x, y), self.rules.source_size());
-                self.grid[(x, y)] = self.rules.update(area.as_slice());
-            }
+        for coord in self.get_coord() {
+            let area = grid_copy.get_area(coord, self.rules.source_size());
+            self.grid[coord] = self.rules.next(area.as_slice());
         }
+    }
+
+    pub fn draw(&self, screen: &mut Window, size: (u32, u32)) {
+        for coord in self.get_coord() {
+            let (r, g, b, a) = self.grid[coord].get_color();
+            screen.set_color(r, g, b, a);
+            screen.fill_rect(Rect::new(coord.0*size.0 as i32, coord.1*size.1 as i32, size.0, size.1));
+        }
+    }
+
+    pub fn print(&self) {
+        self.grid.print();
+    }
+
+    fn get_coord(&self) ->Coord{
+        Coord{width:self.grid.width , height:self.grid.height, x:0,y:0}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,146 @@
 
-use game_of_life::{Game, RuleSet};
-
+use game_of_life::{Game, RuleSet, DataType};
+use simple::{Window,Event, Key};
+use std::time::Duration;
+use std::collections::VecDeque;
 
 
 struct ClassicConeway{}
 
-impl ClassicConeway {}
-
-impl RuleSet<bool> for ClassicConeway {
-    fn update(&self, source: &[&bool]) -> bool {
-        let me = source[4];
-        let all: i32 = source.iter().map(|x| **x as i32).sum();
-        let neighbours = all - *me as i32;
+impl RuleSet<BoolData> for ClassicConeway {
+    fn next(&self, source: &[&BoolData]) -> BoolData {
+        let me = source[4].value;
+        let all: i32 = source.iter().map(|x| x.value as i32).sum();
+        let neighbours = all - me as i32;
 
         match (me, neighbours) {
-            (true, 2) | (_, 3) => true,
-            _ => false
+            (true, 2) | (_, 3) => BoolData{value:true},
+            _ => BoolData{value:false}
         }
     }
 
-    fn source_size(&self) -> u32 {
+    fn source_size(&self) -> u8 {
+        3
+    }
+}
+#[derive(Clone, Copy)]
+struct BoolData {
+    value: bool
+}
+
+impl BoolData {
+    fn rnd() -> BoolData {
+        BoolData{value:rand::random::<bool>()}
+    }
+}
+
+impl DataType for BoolData{
+    fn get_color(&self) -> (u8, u8, u8, u8) {
+        if self.value {
+            (255,255,255,255)
+        }else {
+            (0,0,0,255)
+        }
+    }
+
+    fn get_char(&self) -> char {
+        if self.value {
+            '*'
+        }else {
+            '_'
+        }
+    }
+}
+//**************************************************************
+
+#[derive(Clone)]
+struct BoolHist{
+    current: bool,
+    history: VecDeque<bool>
+}
+
+impl BoolHist {
+    fn rnd() -> BoolHist {
+        BoolHist {
+            history: VecDeque::from(vec![false; 10]),
+            current: rand::random::<bool>(),
+        }
+    }
+}
+
+impl DataType for BoolHist {
+    fn get_color(&self) -> (u8, u8, u8, u8) {
+        if self.current {
+            (255,255,255,255)
+        } else {
+            let s:i32 = self.history.iter().map(|x|*x as i32).sum();
+            let gray = (s * 15) as u8;
+            (gray,gray,gray,255)
+        }
+    }
+    fn get_char(&self) -> char {
+        if self.current {
+            '*'
+        } else {
+            '_'
+        }
+    }
+}
+
+struct ClassicHistory{}
+
+impl RuleSet<BoolHist> for ClassicHistory{
+    fn next(&self, source: &[&BoolHist]) -> BoolHist {
+        let me = source[4];
+        let all: i32 = source.iter().map(|x| x.current as i32).sum();
+        let neighbours = all - me.current as i32;
+
+        let current = match (me.current, neighbours) {
+            (true, 2) | (_, 3) => true,
+            _ => false
+        };
+        let mut history = me.history.clone();
+        history.push_back(me.current);
+        history.pop_front();
+        BoolHist{current, history }
+    }
+
+    fn source_size(&self) -> u8 {
         3
     }
 }
 
+
+
 fn main() {
-    let rules = ClassicConeway{};
-    let mut game = Game::new(&[false; 300 * 300],300,rules).unwrap();
-    game.next();
+    const WIDTH:u32 = 800;
+    const HEIGHT:u32 = 600;
+    const NUMBERS:(u32, u32) =(160,120);
+    const TOTAL:usize = (NUMBERS.0*NUMBERS.1) as usize;
+    const SIZE: (u32,u32) =(WIDTH/NUMBERS.0, HEIGHT/NUMBERS.1);
+    let mut init_data = Vec::with_capacity(TOTAL);
+    for _ in 0..TOTAL{
+        init_data.push(BoolHist::rnd());
+    }
+    let rules = ClassicHistory{};
+    let mut game = Game::new(&init_data, NUMBERS.0, rules).unwrap();
+    let mut screen = Window::new("Game of life", WIDTH as u16, HEIGHT as u16);
+
+
+    while screen.next_frame() {
+        while screen.has_event() {
+            match screen.next_event() {
+                Event::Keyboard { is_down:true, key:Key::Escape } => screen.quit(),
+                _=>()
+            }
+
+        }
+        screen.clear();
+        game.draw(&mut screen, SIZE);
+        game.next();
+
+        std::thread::sleep(Duration::from_millis(50));
+
+        
+    }
 }
