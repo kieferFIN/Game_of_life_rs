ad136d636942e11c46195d585f971e9fc6e3658c
kiefer <eero.manty@gmail.com>
2020-06-06 18:28:04 +0300

    Game done, not tested

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,83 @@
 use std::ops::{Index, IndexMut};
 
+type IndexType = (i32, i32);
+
 #[derive(Clone)]
 struct Grid<D> {
-    width: usize,
-    height: usize,
+    width: u32,
+    height: u32,
     data: Vec<D>,
+
 }
 
 impl<D: Clone> Grid<D> {
-    pub fn init(init_data: &[D], width: usize) -> Option<Grid<D>> {
-        let size = init_data.len() ;
+    pub fn init(init_data: &[D], width: u32) -> Option<Grid<D>> {
+        let size = init_data.len() as u32;
         if size % width != 0 {
             None
         } else {
             Some(Grid { width, height: size / width, data: init_data.to_vec() })
         }
     }
+
+    fn wrap(&self, index: IndexType) -> (usize, usize) {
+        (index.0.rem_euclid( self.width as i32) as usize, index.1.rem_euclid( self.width as i32) as usize)
+    }
+
+    fn get_area(&self, index: IndexType, size: u32) -> Vec<&D> {
+        let mut v = Vec::new();
+        let x = index.0 as i32;
+        let y = index.1 as i32;
+        let half_size = (size / 2) as i32;
+        for h in y - half_size..=y + half_size {
+            for w in x - half_size..=x + half_size {
+                v.push(self.index((w, h)));
+            }
+        }
+        v
+    }
 }
 
-impl<D> Index<(usize, usize)> for Grid<D> {
+impl<D: Clone> Index<IndexType> for Grid<D> {
     type Output = D;
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        let x = index.0 % self.width;
-        let y = index.1 % self.height;
-
-        &self.data[y * (self.width as usize) * x]
+    fn index(&self, index: IndexType) -> &Self::Output {
+        let (x, y) = self.wrap(index);
+        &self.data[y * (self.width as usize) + x]
     }
 }
 
-impl<D> IndexMut<(usize, usize)> for Grid<D>{
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        let x = index.0 % self.width;
-        let y = index.1 % self.height;
-
-        &mut self.data[y * (self.width as usize) * x]
+impl<D: Clone> IndexMut<IndexType> for Grid<D> {
+    fn index_mut(&mut self, index: IndexType) -> &mut Self::Output {
+        let (x, y) = self.wrap(index);
+        &mut self.data[y * (self.width as usize) + x]
     }
 }
 
-
-pub struct Game<D> {
-    grid: Grid<D>
+pub trait RuleSet<D>{
+    fn update(&self, source:&[&D]) -> D;
+    fn source_size(&self) -> u32;
 }
 
-impl<D: Clone> Game<D> {
-    pub fn new(init_data: &[D], width: usize) -> Option<Game<D>> {
-        Grid::init(init_data, width).map_or(None, |grid| Some(Game { grid }))
+
+pub struct Game<D, R>
+    where R: RuleSet<D>{
+    grid: Grid<D>,
+    rules: R,
+}
+
+impl<D: Clone, R> Game<D, R>
+    where R: RuleSet<D> {
+    pub fn new(init_data: &[D], width: u32, rules: R) -> Option<Game<D,R>> {
+        Grid::init(init_data, width).map_or(None, |grid| Some(Game { grid, rules }))
+    }
+
+    pub fn next(&mut self) {
+        let grid_copy = self.grid.clone();
+        for y in 0..grid_copy.height as i32{
+            for x in 0..grid_copy.width as i32{
+                let area = grid_copy.get_area((x, y), self.rules.source_size());
+                self.grid[(x, y)] = self.rules.update(area.as_slice());
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 
-use game_of_life::{Game};
+use game_of_life::{Game, RuleSet};
 
 
 
+struct ClassicConeway{}
+
+impl ClassicConeway {}
+
+impl RuleSet<bool> for ClassicConeway {
+    fn update(&self, source: &[&bool]) -> bool {
+        let me = source[4];
+        let all: i32 = source.iter().map(|x| **x as i32).sum();
+        let neighbours = all - *me as i32;
+
+        match (me, neighbours) {
+            (true, 2) | (_, 3) => true,
+            _ => false
+        }
+    }
+
+    fn source_size(&self) -> u32 {
+        3
+    }
+}
 
 fn main() {
-    let game = Game::new(&[false; 600 * 600],600).unwrap();
+    let rules = ClassicConeway{};
+    let mut game = Game::new(&[false; 300 * 300],300,rules).unwrap();
+    game.next();
 }
