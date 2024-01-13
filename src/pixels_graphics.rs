b485b168824878ab16dd759fae4c2b6219e68241
Eero Mänty <eero.manty@gmail.com>
2024-01-13 02:41:16 +0200

    pixels updated

--- src/pixels_graphics.rs
+++ src/pixels_graphics.rs
@@ -1,22 +1,22 @@
-use winit::event_loop::{EventLoop, ControlFlow};
-use winit::event::{Event, VirtualKeyCode};
-use winit_input_helper::WinitInputHelper;
 use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::keyboard::KeyCode;
 use winit::window::WindowBuilder;
-use winit::platform::run_return::EventLoopExtRunReturn;
+use winit_input_helper::WinitInputHelper;
 
-use pixels::{SurfaceTexture, Pixels};
+use pixels::{Pixels, SurfaceTexture};
 
-use crate::{RuleSet, ColoredDataType, Game, GError};
-use crate::GError::WinitError;
 use crate::error_handling::GError::PixelsError;
+use crate::GError::WinitError;
+use crate::{ColoredDataType, GError, Game, RuleSet};
 
-
-
-pub fn run<R>(window_size: (u32, u32), game: &mut Game<R>) -> Result<(), GError>
-    where R: RuleSet,
-          R::Data: ColoredDataType{
-    let mut event_loop =EventLoop::new();
+pub fn run<R>(window_size: (u16, u16), game: &mut Game<R>) -> Result<(), GError>
+where
+    R: RuleSet,
+    R::Data: ColoredDataType,
+{
+    let event_loop = EventLoop::new().map_err(|e| WinitError { source: e.into() })?;
     let mut input = WinitInputHelper::new();
     let window = {
         let size = LogicalSize::new(window_size.0 as f64, window_size.1 as f64);
@@ -31,29 +31,58 @@ pub fn run<R>(window_size: (u32, u32), game: &mut Game<R>) -> Result<(), GError>
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(game.grid.width as u32, game.grid.height as u32, surface_texture).map_err(|e| PixelsError {source:e.into()})?
+        Pixels::new(
+            game.grid.width as u32,
+            game.grid.height as u32,
+            surface_texture,
+        )
+        .map_err(|e| PixelsError { source: e.into() })?
     };
+    let mut is_paused = true;
     let mut possible_error = None;
 
-    event_loop.run_return( |event,_,control_flow|{
-        if let Event::RedrawRequested(_) = event{
-            pixels.get_frame().copy_from_slice(&game.to_raw_colors());
-            let error = pixels.render().err();
-            if error.is_some(){
-                *control_flow = ControlFlow::Exit;
-                possible_error = error.map(|e|PixelsError {source:e.into()});
-                return;
-            }
-            if input.update(&event){
-                if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                    *control_flow = ControlFlow::Exit;
+    event_loop
+        .run(|event, elwt| match event {
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+                pixels.frame_mut().copy_from_slice(&game.to_raw_colors());
+                let error = pixels.render().err();
+                if error.is_some() {
+                    elwt.exit();
+                    possible_error = error.map(|e| PixelsError { source: e.into() });
                     return;
                 }
-                window.request_redraw();
             }
-        }
-    });
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => elwt.exit(),
+            _ => {
+                if input.update(&event) {
+                    if !is_paused {
+                        game.next_step();
+                    }
 
-    possible_error.map_or(Ok(()),|e|Err(e))
+                    if input.key_pressed(KeyCode::Escape)
+                        || input.close_requested()
+                        || input.destroyed()
+                    {
+                        elwt.exit();
+                        return;
+                    }
+                    if input.key_pressed(KeyCode::Space) {
+                        is_paused ^= true;
+                    }
+                    if input.key_pressed(KeyCode::ArrowRight) && is_paused {
+                        game.next_step();
+                    }
+                    window.request_redraw();
+                }
+            }
+        })
+        .map_err(|e| WinitError { source: e.into() })?;
 
+    possible_error.map_or(Ok(()), |e| Err(e))
 }
